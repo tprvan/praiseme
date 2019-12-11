538e6cf34f8ce773fd3da16c54427167b782e5d8
Tania Prvan <tania.prvan@mq.edu.au>
2019-12-11 15:44:28 +1100

    Adds glue package instead of paste0

--- R/praise.R
+++ R/praise.R
@@ -4,13 +4,13 @@
 #'
 #' @param name Name of person to praise.
 #'
-#' @param punctuation This is our emphasis as a text output.
+#' @param text string, punctuation This is our emphasis as a text output. default is "!"
 #'
 #' @return Text string with praise
 #' @export
 #'
 #' @examples
 #' praise(name="Steph",punctuation="!")
-praise<-function(name,punctuation){
-  paste0("You're the best, ",name,punctuation)
+praise<-function(name,punctuation="!"){
+  glue::glue("You're the best, {name}{punctuation}")
 }
